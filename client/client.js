c91642579e4890359f95dc5421b9deee194642bd
janekkkk <janekozga1@gmail.com>
2014-11-20 14:09:53 +0100

    added categories

--- client/client.js
+++ client/client.js
@@ -4,34 +4,50 @@ Meteor.startup(function () {
   angular.bootstrap(document, ['demo']);
 });
 
+app.filter('unique', function() {
+  return function (arr, field) {
+    return _.uniq(arr, function(a) { return a[field]; });
+  };
+});
+
+var inArray = function(item, array){
+  if(array.indexOf(item) !== -1){
+    return true;
+  }
+  else return false;
+};
+
 app.controller("MainCtrl", ['$scope', '$collection', function($scope, $collection){
 
   $scope.newBlog = {};
   $collection(Posts).bind($scope, 'posts', true, true);
+  $collection(Authors).bind($scope, 'authors', true, true);
+  $collection(Categories).bind($scope, 'categories', true, true);
 
-
-$scope.openModal = function(){
-  $('#addModal').foundation('reveal', 'open');
-}
+  $scope.openModal = function(){
+    $('#addModal').foundation('reveal', 'open');
+  }
 
   $scope.addBlogPost = function(){
     $('#addModal').foundation('reveal', 'close');
     var date = new Date();
     var newDate = "";
-    var newDate = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+    var newDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+
     $scope.newBlog.createdAt = newDate;
+
     $scope.posts.push($scope.newBlog);
     $scope.newBlog = {};
   }
 
 
   $scope.removeBlogPost = function(post){
-     $scope.posts.splice($scope.posts.indexOf(post), 1 );
-  }
+   $scope.posts.splice($scope.posts.indexOf(post), 1 );
+ }
 
-  $scope.getRandomInt = function(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
+ $scope.getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 }]);
 
--- server/server.js
+++ server/server.js
@@ -1,18 +1,33 @@
-//   Meteor.startup(function () {
-//     if (Posts.find().count() === 0) {
+  Meteor.startup(function () {
+    if (Authors.find().count() === 0) {
 
-//         var posts = [
-//         {'title': 'Dubstep-Free Zone',
-//         'content': 'Fast just got faster with Nexus S.'},
-//         {'title': 'All dubstep all the time',
-//         'content': 'Get it on!'},
-//         {'title': 'Savage lounging',
-//         'content': 'Leisure suit required. And only fiercest manners.'}
-//         ];
+        var authors = [
+        {'name': 'Tim'},
+        {'name': 'Ben'},
+        {'name': 'Jelle'},
+        {'name': 'Bjorn'},
+        {'name': 'Janek'}
+        ];
 
-//     //     for (var i = 0; i < posts.length; i++)
-//     //         Posts.insert({title: posts[i].title, content: posts[i].content});
+        authors.forEach(function (e, i) {
+           Authors.insert({name: e.name});
+       });
+    }
 
-//     // }
-// });
+      if (Categories.find().count() === 0) {
+
+        var categories = [
+        {'name': 'Cats'},
+        {'name': 'Code'},
+        {'name': 'Design'},
+        {'name': 'Drumsticks'},
+        {'name': 'Flowers'}
+        ];
+
+        categories.forEach(function (e, i) {
+           Categories.insert({name: e.name});
+       });
+    }
+});
+
 
