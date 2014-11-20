eb484f16d16b131cd724ece3fe8ffc33bb1df2e7
janekkkk <janekozga1@gmail.com>
2014-11-20 17:13:14 +0100

    added authentication rules

---
+++ client/js/client.js
@@ -0,0 +1,64 @@
+var app = angular.module('demo',['angular-meteor','ui.router']);
+
+Meteor.startup(function () {
+  angular.bootstrap(document, ['demo']);
+});
+
+app.filter('unique', function() {
+  return function (arr, field) {
+    return _.uniq(arr, function(a) { return a[field]; });
+  };
+});
+
+var inArray = function(item, array){
+  if(array.indexOf(item) !== -1){
+    return true;
+  }
+  else return false;
+};
+
+app.controller("MainCtrl", ['$scope', '$collection', function($scope, $collection){
+
+  $scope.newBlog = {};
+  $scope.filterCategory = "";
+  $collection(Posts).bind($scope, 'posts', true, true);
+  $collection(Authors).bind($scope, 'authors', true, true);
+  $collection(Categories).bind($scope, 'categories', true, true);
+
+  $scope.selectCategory = function(category){
+    if(category === undefined){
+      $scope.filterCategory = "";
+    }
+    $scope.filterCategory = category.name;
+  }
+
+  $scope.openModal = function(){
+    $('#addModal').foundation('reveal', 'open');
+  }
+
+  $scope.addBlogPost = function(){
+    $('#addModal').foundation('reveal', 'close');
+    var date = new Date();
+    var newDate = "";
+    var newDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+
+    $scope.newBlog.createdAt = newDate;
+
+    $scope.posts.push($scope.newBlog);
+    $scope.newBlog = {};
+  }
+
+
+  $scope.removeBlogPost = function(post){
+   $scope.posts.splice($scope.posts.indexOf(post), 1 );
+ }
+
+ $scope.getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+}]);
+
+
+
+
--- model/posts.js
+++ model/posts.js
@@ -1,1 +1,21 @@
   Posts = new Mongo.Collection("posts");
+
+  Posts.allow({
+    insert: function (userId, doc) {
+       return userId;
+
+   },
+   update: function (userId, doc, fields, modifier) {
+    if (userId !== doc.owner)
+      return false;
+
+  return true;
+},
+remove: function (userId, doc) {
+    if (userId !== doc.owner)
+      return false;
+
+  return true;
+}
+
+});
