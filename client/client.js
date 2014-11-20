13993be49d751c8d82529f9a55f625835249840b
janekkkk <janekozga1@gmail.com>
2014-11-20 11:35:43 +0100

    Added SASS and Underscore.js

--- client/client.js
+++ client/client.js
@@ -1,4 +1,3 @@
-
 var app = angular.module('demo',['angular-meteor']);
 
 Meteor.startup(function () {
@@ -7,17 +6,27 @@ Meteor.startup(function () {
 
 app.controller("MainCtrl", ['$scope', '$collection', function($scope, $collection){
 
-
+  $scope.newBlog = {};
   $collection(Posts).bind($scope, 'posts', true, true);
 
 
-  $scope.newBlog = {};
+$scope.openModal = function(){
+  $('#addModal').foundation('reveal', 'open');
+}
 
   $scope.addBlogPost = function(){
     $('#addModal').foundation('reveal', 'close');
-    $scope.newBlog.createdAt = new Date();
+    var date = new Date();
+    var newDate = "";
+    var newDate = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+    $scope.newBlog.createdAt = newDate;
     $scope.posts.push($scope.newBlog);
     $scope.newBlog = {};
+  }
+
+
+  $scope.removeBlogPost = function(post){
+     $scope.posts.splice($scope.posts.indexOf(post), 1 );
   }
 
   $scope.getRandomInt = function(min, max) {
--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
-  Meteor.startup(function () {
-    if (Posts.find().count() === 0) {
+//   Meteor.startup(function () {
+//     if (Posts.find().count() === 0) {
 
-        var posts = [
-        {'title': 'Dubstep-Free Zone',
-        'content': 'Fast just got faster with Nexus S.'},
-        {'title': 'All dubstep all the time',
-        'content': 'Get it on!'},
-        {'title': 'Savage lounging',
-        'content': 'Leisure suit required. And only fiercest manners.'}
-        ];
+//         var posts = [
+//         {'title': 'Dubstep-Free Zone',
+//         'content': 'Fast just got faster with Nexus S.'},
+//         {'title': 'All dubstep all the time',
+//         'content': 'Get it on!'},
+//         {'title': 'Savage lounging',
+//         'content': 'Leisure suit required. And only fiercest manners.'}
+//         ];
 
-        for (var i = 0; i < posts.length; i++)
-            Posts.insert({title: posts[i].title, content: posts[i].content});
+//     //     for (var i = 0; i < posts.length; i++)
+//     //         Posts.insert({title: posts[i].title, content: posts[i].content});
 
-    }
-});
+//     // }
+// });
 
